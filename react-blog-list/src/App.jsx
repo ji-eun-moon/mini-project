7429f951022e22228cd75d718a77da2b0b43d9e7
문지은 <mjieun0956@gmail.com>
2023-07-25 02:06:15 +0900

    [react-blog-list] 블로그 생성 폼 만들기

--- react-blog-list/src/App.jsx
+++ react-blog-list/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import { useState } from 'react';
 
+import axios from 'axios';
+
 function App() {
   const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const onSubmit = () => {
+    axios.post('http://localhost:3001/posts', {
+      title: title,
+      body: body
+    })
+  }
+
   return (
     <div className='container'>
       <div className='mb-3'>
@@ -15,7 +26,21 @@ function App() {
           }}
         />
       </div>
-      <button className='btn btn-primary'>Post</button>
+      <div className='mb-3'>
+        <label className='form-label'>Body</label>
+        <textarea 
+          className='form-control'
+          value={body}
+          onChange={(event) => {
+            setBody(event.target.value)
+          }}
+          rows={20}
+        />
+      </div>
+      <button 
+          className='btn btn-primary'
+          onClick={onSubmit}
+          >Post</button>
     </div>
   )
 }
