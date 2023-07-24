61f9735ce283c038e87300bfb4da6f5608a0fc91
문지은 <mjieun0956@gmail.com>
2023-07-24 17:31:53 +0900

    [react-blog-list] Input State 바인딩

---
+++ react-blog-list/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState } from 'react';
+
+function App() {
+  const [title, setTitle] = useState('');
+  return (
+    <div className='container'>
+      <div className='mb-3'>
+        <label className='form-label'>Title</label>
+        <input 
+          className='form-control'
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value)
+          }}
+        />
+      </div>
+      <button className='btn btn-primary'>Post</button>
+    </div>
+  )
+}
+
+export default App;
